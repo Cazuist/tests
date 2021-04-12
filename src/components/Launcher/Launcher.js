1d136175f88b290903e83630f307c8a7ce2fdb96
Cazuist <sovrus@mail.ru>
2021-04-12 14:53:31 +0300

    Commit 12.04-2

--- src/components/Launcher/Launcher.js
+++ src/components/Launcher/Launcher.js
@@ -1,29 +1,60 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import TableContext from '../../contexts/TableContext';
 
 export default function Launcher() {
+  const [ isValid, setValidationStatus ] = useState(true);
+  const [ validationMsg, setValidationMsg ] = useState(' валиден!');
+
   const { testConfig, setTestConfig } = useContext(TableContext);
 
-  const onTextChange = (e) => {
+  const onFieldFocus = (e) => {
     e.target.style.height = `${e.target.scrollHeight}px`;
-    setTestConfig(e.target.value);  
   }
 
-  const onTextBlur = (e) => {
-    e.target.style.height = '250px';    
+  const onFieldBlur = (e) => {
+    e.target.style.height = '250px';
+  }
+
+  const onConfigChange = (e) => {    
+    setTestConfig(e.target.value);
+    
+    if(!e.target.value.trim()) {
+      e.target.style.height = '250px';
+      setValidationStatus(false);
+      setValidationMsg(' отсутствует!');
+      return;
+    }
+
+    try {
+      JSON.parse(e.target.value);
+      setValidationStatus(true);
+      setValidationMsg(' валиден!');
+    } catch(e) {
+      setValidationStatus(false);
+      setValidationMsg(e.message);
+    }    
   }
 
   return (
-    <div className="launcher__box">
-      <div className="test-launcher__box">
-        <button className='launcher__btn'>Запустить тесты</button>
+    <div className="test-launcher__box">
+      <div className="launcher__btn__box">
+        <button className='launcher__btn launch_btn'>Запустить тесты</button>
+        
+
+        <div className={`validation__status${isValid ? ' valid' : ' invalid'}`}>
+          <span>Файл конфигурации: {`${validationMsg}`}</span>
+        </div>
       </div>
 
       <textarea value={testConfig}
-          className="config__box" 
-          onFocus={onTextChange}
-          onInput={onTextChange}
-          onBlur={onTextBlur} />
+          className={`config__box${isValid ? ' valid' : ' invalid'}`}
+          onFocus={onFieldFocus}
+          onInput={onConfigChange}
+          onBlur={onFieldBlur} />
     </div>
   );
 }
+
+/*<button className='launcher__btn validate_btn'
+          onClick={validateJSON}
+        >Проверить Config</button>*/
